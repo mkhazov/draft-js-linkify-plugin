1581b4fabb3d20d5c6cb3d5422baaafc4a88554c
Mikhail Khazov <mkhazov.work@gmail.com>
2017-10-20 13:34:43 +0300

    chore: react@16.0.0

--- src/LinkAdd/index.js
+++ src/LinkAdd/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { getVisibleSelectionRect } from 'draft-js';
 import linkifyIt from 'linkify-it';
